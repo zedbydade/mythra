41aa72e95e5e5f8b3250c59d6e769d4cc3186150
Francisco Paradela <franciscoleite.dev@protonmail.com>
2024-06-17 19:42:45 -0300

    Add multi-thread channels receivers

--- lib/storage/memory.rb
+++ lib/storage/memory.rb
@@ -1,24 +1,43 @@
 require 'concurrent-edge'
+require 'zlib'
 require_relative 'mod'
 
 class Memory
-  attr_accessor :map, :dirty
+  attr_accessor :map, :path, :atomic_bool, :handler, :stop_channel
 
-  def initialize(map, dirty)
-    @map = map
-    @dirty = dirty
+  include Mod
+
+  def initialize(path)
+    @path = path
+    @map = Zlib::GzipReader.open(get_location(path)).read
+    @atomic_bool = Concurrent::AtomicBoolean.new(false)
+    @handler = nil
+    @stop_channel = nil
   end
 
-  def self.call(path)
-    map = Zlib::GzipReader.open(get_location(path)).read
-    atomic_bool = Concurrent::AtomicBoolean(false)
-    channel = Concurrent::Channel.new
-    ticket = Concurrent::Channel.ticket(0.5)
+  def call
+    @stop_channel = Concurrent::Channel.new
+    ticker = Concurrent::Channel.ticker(0.5)
+    @handler = Thread.new do
+      # rubocop:disable Lint/UnreachableLoop
+      loop do
+        Concurrent::Channel.select do |s|
+          s.take(ticker) do
+            save(@path, @map) if @atomic_bool.true?
+            @atomic_bool.make_false
+          end
+          s.take(@stop_channel) { break }
+        end
+      end
+      # rubocop:enable Lint/UnreachableLoop
+    end
   end
 
   private
 
-  def dirty?(atomic_bool)
-    atomic_bool.false?
+  def save(location, map)
+    Zlib::GzipWriter.open(location) do |gz|
+      gz.write map
+    end
   end
 end
