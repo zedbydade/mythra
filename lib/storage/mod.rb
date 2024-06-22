b6d2ed8a2a0dacf54825931b02408498c2eb411f
Francisco Paradela <franciscoleite.dev@protonmail.com>
2024-06-21 23:57:02 -0300

    Add upsert method

--- lib/storage/mod.rb
+++ lib/storage/mod.rb
@@ -1,6 +1,17 @@
 require 'pathname'
+require_relative 'memory'
 
 module Mod
+  def upsert(ip:, expire_until:, memory:)
+    map = memory.read_map
+
+    memory.map = if map[ip]
+                   map[ip]['until'] = expire_until
+                 else
+                   map[ip] = { 'until' => expire_until, times: 0 }
+                 end
+  end
+
   def get_location(path)
     return path if path.is_a? Pathname
 
